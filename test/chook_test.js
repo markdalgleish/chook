55471b2a5d15a20115344036be6548fd2fb82b4c
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-07-19 08:54:49 +1000

    Initial test

--- test/chook_test.js
+++ test/chook_test.js
@@ -1,34 +1,34 @@
-var jstd_runner = require('../lib/jstd-runner.js');
+var chook = require('../lib/chook.js');
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+	======== A Handy Little Nodeunit Reference ========
+	https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
+	Test methods:
+		test.expect(numAssertions)
+		test.done()
+	Test assertions:
+		test.ok(value, [message])
+		test.equal(actual, expected, [message])
+		test.notEqual(actual, expected, [message])
+		test.deepEqual(actual, expected, [message])
+		test.notDeepEqual(actual, expected, [message])
+		test.strictEqual(actual, expected, [message])
+		test.notStrictEqual(actual, expected, [message])
+		test.throws(block, [error], [message])
+		test.doesNotThrow(block, [error], [message])
+		test.ifError(value)
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(jstd_runner.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  }
+exports['core'] = {
+	setUp: function(done) {
+		done();
+	},
+	'has public functions': function(test) {
+		test.expect(3);
+		test.equal(typeof chook.configure, 'function', 'should have a "configure" function.');
+		test.equal(typeof chook.use, 'function', 'should have a "use" function.');
+		test.equal(typeof chook.run, 'function', 'should have a "run" function.');
+		test.done();
+	}
 };
