f5e075fd30839c74193fc9e140ddaf1a997a6344
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-07-20 09:02:39 +1000

    Add more tests, refactor

--- test/chook_test.js
+++ test/chook_test.js
@@ -20,7 +20,7 @@ var chook = require('../lib/chook.js');
 		test.ifError(value)
 */
 
-exports['core'] = {
+exports['chook'] = {
 	setUp: function(done) {
 		done();
 	},
@@ -29,6 +29,16 @@ exports['core'] = {
 		test.equal(typeof chook.configure, 'function', 'should have a "configure" function.');
 		test.equal(typeof chook.use, 'function', 'should have a "use" function.');
 		test.equal(typeof chook.run, 'function', 'should have a "run" function.');
+		test.done();
+	},
+	'has reporters': function(test) {
+		test.expect(1);
+		test.equal(typeof chook.reporters, 'object', 'should have reporters property');
+		test.done();
+	},
+	'has browsers': function(test) {
+		test.expect(1);
+		test.equal(typeof chook.browsers, 'object', 'should have browsers property');
 		test.done();
 	}
 };
